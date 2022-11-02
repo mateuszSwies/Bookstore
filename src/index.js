8ab9f40afb305afffe9eb6897e9339be54439c66
Mateusz Swies <mateusz1swies@gmail.com>
2022-11-02 13:18:06 +0100

    add function to change active el in lists

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+//import bootstrap
 import "bootstrap/dist/js/bootstrap";
+
+//download items from page
+const testjs = document.querySelector("#firstMainHeader");
+const sideMenuList = [...document.querySelectorAll(".list-group a")];
+const pagination = [...document.querySelectorAll(".pagination li")];
+
+//functions
+
+const changeActiveElement = (selector) => {
+	selector.forEach((item) =>
+		item.addEventListener("click", () => {
+			selector.forEach((item) => {
+				item.classList.remove("active");
+			});
+			item.classList.add("active");
+		})
+	);
+};
+
+//caling functions
+changeActiveElement(sideMenuList);
+changeActiveElement(pagination);
